96efae727f32b526b7b1885db2549a3ea8c36046
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-13 01:08:35 +0530

    auto push

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public_subnet" {
-  count            = length(var.PUBLIC_SUBNET_CIDR)                     # it will read number of cidr are there accordingly it will create that many number of subnet
-  vpc_id           = aws_vpc.roboshop_vpc.id
-  cidr_block       = element(var.PUBLIC_SUBNET_CIDR, count.index)      # element will pick the value from list 
+  count                    = length(var.PUBLIC_SUBNET_CIDR)                     # it will read number of cidr are there accordingly it will create that many number of subnet
+  vpc_id                   = aws_vpc.roboshop_vpc.id
+  cidr_block               = element(var.PUBLIC_SUBNET_CIDR, count.index)      # element will pick the value from list 
+  map_public_ip_on_launch  = true
 
   tags = {
     Name = "roboshop-${var.ENV}-public-subnet-${count.index}"
