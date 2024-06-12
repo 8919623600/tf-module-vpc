700e8be1f8dd542c0d98d8022dda71e03dd5a487
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-13 00:43:06 +0530

    auto push

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "roboshop_vpc" {
-  cidr_block       = var.VPC_CIDR
-  instance_tenancy = "default"
+  cidr_block               = var.VPC_CIDR
+  instance_tenancy         = "default"
+  enable_dns_hostnames     = "true"
 
   tags = {
     Name = "roboshop-${var.ENV}-vpc"
   }
+  
 }
+
