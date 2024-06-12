9945ed60457a926a9e603377d97c03248f4848a0
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-13 01:57:54 +0530

    auto push

--- routeTable.tf
+++ routeTable.tf
@@ -1,13 +1,35 @@
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public_subnet_rt_association" {
   vpc_id = aws_vpc.roboshop_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block   = "0.0.0.0/0"
+    gateway_id   = aws_internet_gateway.igw.id
   }
 
   
   tags = {
     Name = "${var.ENV}-public_rt"
   }
+}
+
+resource "aws_route_table" "private_subnet_rt_association" {
+  vpc_id = aws_vpc.roboshop_vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+
+  
+  tags = {
+    Name = "${var.ENV}-private_rt"
+  }
+}
+
+# route table association with public subnet
+
+resource "aws_route_table_association" "private_rt" {
+  count            = lenght(aws_subnet.private_subnet.*.id)
+  subnet_id        = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id   = aws_route_table.public_rt.id
 }
