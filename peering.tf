d18b535d0b188567bcd12c1343d3ccc4c334ed2a
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-13 11:55:48 +0530

    auto push

--- peering.tf
+++ peering.tf
@@ -2,4 +2,5 @@ resource "aws_vpc_peering_connection" "roboshop-default-peering" {
 #   peer_owner_id = var.peer_owner_id        Though both vpc's are in same account so dont need this
   peer_vpc_id   = aws_vpc.roboshop_vpc.id
   vpc_id        = var.DEFAULT_VPC_ID
+  auto_accept   = true
 }
