84f9040a4edcaf2f434f2b8db86742332d38b3a0
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-06-14 18:24:20 +0530

    auto push

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "public_subnet" {
+output "PUBLIC_SUBNET_IDS" {
     value = aws_subnet.public_subnet.*.id
 
   }
 
-   output "private_subnet_id" {
-    value = aws_subnet.private_subnet.*.id
-  }
+#    output "private_subnet_id" {
+#     value = aws_subnet.private_subnet.*.id
+#   }
