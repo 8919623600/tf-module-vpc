958c57a7d0aba40ed1a79e710e7c5f2ac58b32b3
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-14 22:00:29 +0530

    auto push

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "PUBLIC_SUBNET_IDS" {
 
   }
 
-#    output "private_subnet_id" {
-#     value = aws_subnet.private_subnet.*.id
-#   }
+output "PRIVATE_SUBNET_IDS" {
+    value = aws_subnet.private_subnet.*.id
+  }
