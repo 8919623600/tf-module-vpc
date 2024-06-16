ac6f014432e1b34d132e2e569b8f81418b97c992
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-16 15:29:09 +0530

    auto push

--- vars.tf
+++ vars.tf
@@ -11,4 +11,53 @@ variable "DEFAULT_VPC_RT_ID" {}
 
 
 
+variable  "ingress_rule" {
+    default = [
+    {
+      description     = "ssh"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      
+    },
+    {
+      description     = "web"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      
+  },
+     {
+      description     = "jenkins"
+      from_port       = 8080
+      to_port         = 8080
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+      
+  },
+     {
+      description        = "nexus"
+      from_port          = 8081
+      to_port            = 8081
+      protocol           = "tcp"
+      cidr_blocks        = ["0.0.0.0/0"]
+      
+  }
+    ]
+} 
+
+
+variable "egress_rule" {
+    default = [ {
+      description        = "egress all"
+      from_port          = 0
+      to_port            = 0
+      protocol           = "tcp"
+      cidr_blocks        = ["0.0.0.0/0"]
+      
+    },
+    ]
+}
 
