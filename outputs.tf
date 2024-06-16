f069458ee7cfde4d670fbe523bc61783548f55dd
Jyoti Ranjan Sahoo <jyoti@Jyotis-MacBook-Air.local>
2024-06-17 02:20:51 +0530

    auto push

--- outputs.tf
+++ outputs.tf
@@ -30,5 +30,10 @@ output "PUBLIC_SUBNET_CIDR" {
     value = aws_subnet.public_subnet.*.cidr_block
 }
 
+output "DEFAULT_VPC_ID" {
+    value = var.DEFAULT_VPC_ID
+}   
 
-
+output "DEFAULT_VPC_CIDR" {
+    value = var.DEFAULT_VPC_CIDR
+}
