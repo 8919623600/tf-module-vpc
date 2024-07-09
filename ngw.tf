24a7695c476c6bc593a3526822f6ec22724e419f
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-09 15:20:34 +0530

    updated

--- ngw.tf
+++ ngw.tf
@@ -1,21 +1,21 @@
 # elastic ip
-resource "aws_eip" "ngw_ip" {
-  domain =  "vpc"
+# resource "aws_eip" "ngw_ip" {
+#   domain =  "vpc"
 
-  tags = {
-    Name = "roboshop-${var.ENV}-eip"
-  }
-}
+#   tags = {
+#     Name = "roboshop-${var.ENV}-eip"
+#   }
+# }
 
-resource "aws_nat_gateway" "ng" {
-  allocation_id = aws_eip.ngw_ip.id
-  subnet_id     = aws_subnet.public_subnet.*.id[0]
+# resource "aws_nat_gateway" "ng" {
+#   allocation_id = aws_eip.ngw_ip.id
+#   subnet_id     = aws_subnet.public_subnet.*.id[0]
 
-  tags = {
-    Name = "roboshop-${var.ENV}-ng"
-  }
+#   tags = {
+#     Name = "roboshop-${var.ENV}-ng"
+#   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC and eip.
-  depends_on = [aws_internet_gateway.igw , aws_eip.ngw_ip]
-}
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC and eip.
+#   depends_on = [aws_internet_gateway.igw , aws_eip.ngw_ip]
+# }
--- routeTable.tf
+++ routeTable.tf
@@ -6,10 +6,10 @@ resource "aws_route_table" "public_subnet_rt_association" {
     gateway_id   = aws_internet_gateway.igw.id
   }
 
-  route {
-    cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
-  }
+  # route {
+  #   cidr_block                = var.DEFAULT_VPC_CIDR
+  #   vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
+  # }
 
   
   tags = {
@@ -24,35 +24,35 @@ resource "aws_route_table_association" "public_rt" {
   route_table_id   = aws_route_table.public_subnet_rt_association.id
 }
 
-resource "aws_route_table" "private_subnet_rt_association" {
-  vpc_id = aws_vpc.roboshop_vpc.id
+# resource "aws_route_table" "private_subnet_rt_association" {
+#   vpc_id = aws_vpc.roboshop_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ng.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.ng.id
+#   }
 
-  route {
-    cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
-  }
+#   route {
+#     cidr_block                = var.DEFAULT_VPC_CIDR
+#     vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
+#   }
 
   
   
-  tags = {
-    Name = "${var.ENV}-private_rt"
-  }
-}
+#   tags = {
+#     Name = "${var.ENV}-private_rt"
+#   }
+# }
 
 
-resource "aws_route_table_association" "private_rt" {
-  count            = length(aws_subnet.private_subnet.*.id)
-  subnet_id        = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id   = aws_route_table.private_subnet_rt_association.id
-}
+# resource "aws_route_table_association" "private_rt" {
+#   count            = length(aws_subnet.private_subnet.*.id)
+#   subnet_id        = element(aws_subnet.private_subnet.*.id, count.index)
+#   route_table_id   = aws_route_table.private_subnet_rt_association.id
+# }
 
-resource "aws_route" "r" {
-  route_table_id            = var.DEFAULT_VPC_RT_ID
-  destination_cidr_block    = var.VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
-}
+# resource "aws_route" "r" {
+#   route_table_id            = var.DEFAULT_VPC_RT_ID
+#   destination_cidr_block    = var.VPC_CIDR
+#   vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
+# }
