432430ecf523021606de84d6fbd81b6dcbd634f9
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-10 14:43:47 +0530

    updated

--- routeTable.tf
+++ routeTable.tf
@@ -6,10 +6,10 @@ resource "aws_route_table" "public_subnet_rt_association" {
     gateway_id   = aws_internet_gateway.igw.id
   }
 
-  # route {
-  #   cidr_block                = var.DEFAULT_VPC_CIDR
-  #   vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
-  # }
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
+  }
 
   
   tags = {
