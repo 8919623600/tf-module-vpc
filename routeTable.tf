0fd5ca78ec5523ba4a34c02d860ec964875860ba
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-10 14:49:46 +0530

    updated

--- routeTable.tf
+++ routeTable.tf
@@ -24,25 +24,25 @@ resource "aws_route_table_association" "public_rt" {
   route_table_id   = aws_route_table.public_subnet_rt_association.id
 }
 
-# resource "aws_route_table" "private_subnet_rt_association" {
-#   vpc_id = aws_vpc.roboshop_vpc.id
+resource "aws_route_table" "private_subnet_rt_association" {
+  vpc_id = aws_vpc.roboshop_vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.ng.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ng.id
+  }
 
-#   route {
-#     cidr_block                = var.DEFAULT_VPC_CIDR
-#     vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
-#   }
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.roboshop_default_peering.id
+  }
 
   
   
-#   tags = {
-#     Name = "${var.ENV}-private_rt"
-#   }
-# }
+  tags = {
+    Name = "${var.ENV}-private_rt"
+  }
+}
 
 
 resource "aws_route_table_association" "private_rt" {
